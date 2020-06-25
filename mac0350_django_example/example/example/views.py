9bf985f5167b46c3d322b3fd06b7254eed4a9deb
Rogério Fernandes <rogeriomfneto@hotmail.com>
2020-06-25 18:15:58 -0300

    arquivos restantes adicionados

--- mac0350_django_example/example/example/views.py
+++ mac0350_django_example/example/example/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#from .models import Usuario
-#from .models import Perfil
 from django.db import connection
 from collections import namedtuple
 from django.template import loader
